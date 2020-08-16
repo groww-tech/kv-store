ffcdc90a7cb35eae49fa7e439f5a246fdc715fee
groww-tech <siddharthcwork01@gmail.com>
2020-08-16 16:32:07 -0500

    Add checkTx

--- app.go
+++ app.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"bytes"
+
+	"github.com/dgraph-io/badger"
 	abcitypes "github.com/tendermint/tendermint/abci/types"
 )
 
-type KVStoreApplication struct {}
+type KVStoreApplication struct {
+	db           *badger.DB
+	currentBatch *badger.Txn
+}
 
 var _ abcitypes.Application = (*KVStoreApplication)(nil)
 
-func NewKVStoreApplication() *KVStoreApplication {
-	return &KVStoreApplication{}
+func NewKVStoreApplication(db *badger.DB) *KVStoreApplication {
+	return &KVStoreApplication{
+		db: db,
+	}
 }
 
 func (KVStoreApplication) Info(req abcitypes.RequestInfo) abcitypes.ResponseInfo {
@@ -24,8 +32,41 @@ func (KVStoreApplication) DeliverTx(req abcitypes.RequestDeliverTx) abcitypes.Re
 	return abcitypes.ResponseDeliverTx{Code: 0}
 }
 
-func (KVStoreApplication) CheckTx(req abcitypes.RequestCheckTx) abcitypes.ResponseCheckTx {
-	return abcitypes.ResponseCheckTx{Code: 0}
+func (app *KVStoreApplication) isValid(tx []byte) (code uint32) {
+	// check format
+	parts := bytes.Split(tx, []byte("="))
+	if len(parts) != 2 {
+		return 1
+	}
+
+	key, value := parts[0], parts[1]
+
+	// check if the same key=value already exists
+	err := app.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get(key)
+		if err != nil && err != badger.ErrKeyNotFound {
+			return err
+		}
+		if err == nil {
+			return item.Value(func(val []byte) error {
+				if bytes.Equal(val, value) {
+					code = 2
+				}
+				return nil
+			})
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return code
+}
+
+func (app *KVStoreApplication) CheckTx(req abcitypes.RequestCheckTx) abcitypes.ResponseCheckTx {
+	code := app.isValid(req.Tx)
+	return abcitypes.ResponseCheckTx{Code: code, GasWanted: 1}
 }
 
 func (KVStoreApplication) Commit() abcitypes.ResponseCommit {
